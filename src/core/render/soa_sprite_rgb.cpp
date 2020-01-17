fd353119f3e7d345fffdbf8be5af4fa0726515ba
madwareru <madware.ru@gmail.com>
2020-01-18 01:07:11 +0300

    minor change to ensure that buffers initialized on start

--- src/core/render/soa_sprite_rgb.cpp
+++ src/core/render/soa_sprite_rgb.cpp
@@ -7,6 +7,12 @@ namespace core::render {
         r_buffer_raw_ = new std::uint8_t[w * h + 8];
         g_buffer_raw_ = new std::uint8_t[w * h + 8];
         b_buffer_raw_ = new std::uint8_t[w * h + 8];
+        for(std::size_t i = 0; i < w * h + 8; ++i)
+         {
+             r_buffer_raw_[i] = 0;
+             g_buffer_raw_[i] = 0;
+             b_buffer_raw_[i] = 0;
+         }
 
         r_buffer_ = (reinterpret_cast<std::size_t>(&r_buffer_raw_[0]) % 16 == 0)
             ? &r_buffer_raw_[0]
